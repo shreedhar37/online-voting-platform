abce883dd5ac25a0bd03fd8a73b2628affd89140
Shreedhar Chavan <meshree4@gmail.com>
2022-12-31 19:55:50 +0530

    db migration, test case for new db, use test env

--- __tests__/test.js
+++ __tests__/test.js
@@ -6,3 +6,25 @@ describe("Sample test cases", () => {
     expect(true).toBe(true);
   });
 });
+
+// __tests__/todo.js
+/* eslint-disable no-undef */
+const db = require("../models");
+
+describe("Admin Test Suite", () => {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
+  });
+
+  test("Should add admin", async () => {
+    const adminCounts = await db.Admin.count();
+    await db.Admin.addAdmin({
+      firstName: "Test firstName",
+      lastName: "Test lastName",
+      email: "Test email",
+      password: "Test-pa$$w0rd",
+    });
+    const newAdminCount = await db.Admin.count();
+    expect(newAdminCount).toBe(adminCounts + 1);
+  });
+});
